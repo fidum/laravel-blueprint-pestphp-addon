23a837608f84c4b694c6b01b183443d5af5ea607
dmason30 <danmason@spendology.com>
2020-05-27 04:24:06 +0100

    wip

--- src/Traits/PopulatesTestStub.php
+++ src/Traits/PopulatesTestStub.php
@@ -7,15 +7,9 @@ trait PopulatesTestStub
     private function populateTestStub(string $stub, string $namespace, string $testCases, string $imports = ''): string
     {
         $stub = str_replace('DummyNamespace', $namespace, $stub);
-        $stub = str_replace('// imports...', $imports, $stub);
-        $stub = str_replace('// test cases...', $testCases, $stub);
-        $stub = $this->removeSequentialBlankLines($stub);
+        $stub = str_replace("// imports...\n\n", $imports, $stub);
+        $stub = str_replace("// test cases...", $testCases, $stub);
 
         return $stub;
-    }
-
-    private function removeSequentialBlankLines(string $string): string
-    {
-        return preg_replace('/\r?\n(\s*\r?\n){2,}/', PHP_EOL.PHP_EOL, $string).(windows_os() ? PHP_EOL.PHP_EOL : '');
     }
 }
