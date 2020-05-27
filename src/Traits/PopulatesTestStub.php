f120b4a6faca419b7e066b9cf9a18872b71422a2
dmason30 <danmason@spendology.com>
2020-05-27 04:03:19 +0100

    trim output for windows compat

--- src/Traits/PopulatesTestStub.php
+++ src/Traits/PopulatesTestStub.php
@@ -16,6 +16,6 @@ trait PopulatesTestStub
 
     private function removeSequentialBlankLines(string $string): string
     {
-        return preg_replace('/\r?\n(\s*\r?\n){2,}/', "\n\n", $string);
+        return trim(preg_replace('/\r?\n(\s*\r?\n){2,}/', "\n\n", $string));
     }
 }
