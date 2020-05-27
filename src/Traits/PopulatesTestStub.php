cf3ef78d70b19e9f6279122dc3d287619da92cec
Fidum <fidum.dev@gmail.com>
2020-05-27 14:26:04 +0000

    Apply fixes from StyleCI

--- src/Traits/PopulatesTestStub.php
+++ src/Traits/PopulatesTestStub.php
@@ -9,12 +9,14 @@ trait PopulatesTestStub
         $stub = str_replace('DummyNamespace', $namespace, $stub);
         $stub = str_replace("// imports...\n\n", $imports, $stub);
         $stub = str_replace("// imports...\r\n\r\n", $imports, $stub);
+
         return str_replace('// test cases...', $testCases, $stub);
     }
 
     private function populateTestCaseStub(string $stub, string $description, string $content)
     {
         $stub = str_replace('dummy_test_case', $description, $stub);
+
         return str_replace('// ...', $content, $stub);
     }
 }
