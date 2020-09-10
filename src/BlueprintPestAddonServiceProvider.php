4498b8bf45a3fea5e42539eb64777fd38c87252c
dmason30 <danmason@spendology.com>
2020-09-10 18:24:48 +0100

    Restore psalm laravel plugin using dev-master for now

--- src/BlueprintPestAddonServiceProvider.php
+++ src/BlueprintPestAddonServiceProvider.php
@@ -3,14 +3,13 @@
 namespace Fidum\BlueprintPestAddon;
 
 use Blueprint\Blueprint;
-use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider;
 
 class BlueprintPestAddonServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->extend(Blueprint::class, function (Blueprint $blueprint, Container $app) {
+        $this->app->extend(Blueprint::class, function (Blueprint $blueprint, $app) {
             $blueprint->swapGenerator(
                 \Blueprint\Generators\TestGenerator::class,
                 new PestGenerator($app['files'])
