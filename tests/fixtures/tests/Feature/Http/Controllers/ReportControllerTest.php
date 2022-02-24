a34ff475dc5f5ef63a928a05c42199d0be4b89e7
dmason30 <danmason@spendology.com>
2022-02-24 23:35:33 +0000

    us correct yaml syntax for test

--- tests/fixtures/tests/Feature/Http/Controllers/ReportControllerTest.php
+++ tests/fixtures/tests/Feature/Http/Controllers/ReportControllerTest.php
@@ -4,12 +4,18 @@ namespace Tests\Feature\Http\Controllers;
 
 use App\Events\ExportReport;
 use App\Jobs\GenerateReport;
+use App\Mail\SendReport;
+use App\Notification\ReportGenerated;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Queue;
 
 it('displays view on __invoke', function () {
     Queue::fake();
     Event::fake();
+    Notification::fake();
+    Mail::fake();
 
     $response = $this->get(route('report.__invoke'));
 
@@ -21,5 +27,11 @@ it('displays view on __invoke', function () {
     });
     Event::assertDispatched(ExportReport::class, function ($event) use ($event) {
         return $event->event->is($event);
+    });
+    Notification::assertSentTo($auth->user, ReportGenerated::class, function ($notification) use ($event) {
+        return $notification->event->is($event);
+    });
+    Mail::assertSent(SendReport::class, function ($mail) use ($event) {
+        return $mail->event->is($event);
     });
 });
